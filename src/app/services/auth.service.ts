0e7441ace2b13c42079410c7766a82462f5b904b
Nathanael Cassagnol <nathanaelcassagnol@gmail.com>
2024-11-12 18:20:23 -0500

    testing

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
+import { CreateUser, CreateUserData } from "api/Users";
+import { confirmSignIn, signIn } from 'aws-amplify/auth'
 
 @Injectable({
     providedIn: 'root'
@@ -14,5 +16,27 @@ export class AuthService {
         //https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize?response_type=code&redirect_uri=[redirect_uri]&client_id=[client_id]&state=[state]&aud=[audience]
         const url = `https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize?response_type=code&redirect_uri=${this.RedirectUrl}&client_id=${this.ClientIdNonProduction}`;
         this.http.get(url).subscribe(v => console.log(v));
+    }
+    CreateUser = (input: CreateUserData) => {
+        return CreateUser(input);
+    }
+    SignIn = async (username: string, password: string): Promise<boolean> => {
+        const { isSignedIn, nextStep } = await signIn({
+            username,
+            password,
+        })
+
+        // TODO Implement any required next steps
+        // "CONFIRM_SIGN_IN_WITH_CUSTOM_CHALLENGE" | "CONTINUE_SIGN_IN_WITH_MFA_SELECTION" | "CONFIRM_SIGN_IN_WITH_SMS_CODE" | "CONFIRM_SIGN_IN_WITH_TOTP_CODE" | "CONTINUE_SIGN_IN_WITH_TOTP_SETUP" | "CONFIRM_SIGN_UP" | "RESET_PASSWORD" | "DONE" | "CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED"
+        // if (nextStep.signInStep === 'CONFIRM_SIGN_IN_WITH_CUSTOM_CHALLENGE') {
+        //     await confirmSignIn({
+        //         challengeResponse: "12345"
+        //     })
+        // }
+
+        return isSignedIn;
+    }
+    SignOut = () => {
+        
     }
 }
